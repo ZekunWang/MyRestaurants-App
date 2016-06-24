f01c888ab3afcee8c38c7ddb4d8fcfea9374e504
Zekun Wang <zwang172@gmail.com>
2016-06-24 00:08:18 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# MyRestaurants
+
+MyRestaurants is an android app that uses device's location information to fetch nearby restaurants via Yelp API and get map information of restaurants.
+
+Submitted by: Zekun Wang
+
+The following **required** functionality is completed:
+
+* [x] User can **communicate with Yelp via Yelp API** and get nearby restaurants
+* [x] User can **tap a restaurant in the list and bring up an new screen to show it on map**.
+
+The following **optional** features are implemented:
+
+* [x] Request location information from device
+* [x] Improve style of the restaurants in the list [using a custom adapter]
+* [x] Use fragment instead of new Activity to show list of restaurants
+
+The following **additional** features are implemented:
+* [x] Add support to show all restaurants in the same map
+
+Here's a walkthrough of implemented user stories:
+
+![Video Walkthrough](MyRestaurants.gif)
+
+GIF created with [LiceCap](http://www.cockos.com/licecap/).
+
+Challenges encountered while building the app:
+
+1. Get location information from device
+2. Implement Google map functionalities.
+3. Analysis of response data from Yelp
---
+++ app/src/main/java/com/laioffer/laiofferproject/YelpApi.java
@@ -0,0 +1,45 @@
+package com.laioffer.laiofferproject;
+
+import org.scribe.builder.ServiceBuilder;
+import org.scribe.model.OAuthRequest;
+import org.scribe.model.Response;
+import org.scribe.model.Token;
+import org.scribe.model.Verb;
+import org.scribe.oauth.OAuthService;
+
+public class YelpApi {
+
+    private static final String API_HOST = "api.yelp.com";
+    private static final String SEARCH_PATH = "/v2/search";
+
+    private static final String CONSUMER_KEY = "_OCgRiBEiGZ6s_TWfWxPOw";//"YOUR_CONSUMER_KEY";
+    private static final String CONSUMER_SECRET = "KgkUf9Zz7j7YG0fovpv7cVgWvgw";//"YOUR_CONSUMER_SECRET";
+    private static final String TOKEN = "3zNkfG9ehlVy_LUTKobXhxuB4T2HIdPB";	//"YOUR_TOKEN";
+    private static final String TOKEN_SECRET = "NkpPLDyIQpwNtXmva2W_TiPnxfw";//"YOUR_TOKEN_SECRET";
+
+    private OAuthService service;
+    private Token accessToken;
+
+    /**
+     * Setup the Yelp API OAuth credentials.
+     */
+    public YelpApi() {
+        this.service = new ServiceBuilder().provider(TwoStepOAuth.class).apiKey(CONSUMER_KEY)
+                .apiSecret(CONSUMER_SECRET).build();
+        this.accessToken = new Token(TOKEN, TOKEN_SECRET);
+    }
+
+    /**
+     * Fire a search request to Yelp API.
+     */
+    public String searchForBusinessesByLocation(String term, double lat, double lng, int searchLimit) {
+        OAuthRequest request = new OAuthRequest(Verb.GET, "http://" + API_HOST + SEARCH_PATH);
+        request.addQuerystringParameter("term", term);
+        request.addQuerystringParameter("ll", lat + "," + lng);
+        request.addQuerystringParameter("limit", String.valueOf(searchLimit));
+        this.service.signRequest(this.accessToken, request);
+        Response response = request.send();
+        return response.getBody();
+    }
+
+}
